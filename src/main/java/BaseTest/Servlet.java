4c0e32a73bf3fbcf46980f5770973e7d518eded9
Ciper <ciperchou@qq.com>
2019-03-07 10:36:30 +0800

    AscentSystem -- DataBase connect and data import complete

---
+++ src/main/java/BaseTest/Servlet.java
@@ -0,0 +1,24 @@
+package BaseTest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@javax.servlet.annotation.WebServlet(name = "Servlet")
+public class Servlet extends javax.servlet.http.HttpServlet {
+    // Post与Get的具体区别
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {}
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html"); //设置返回文件类型
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>HelloWorld</title><head>");
+        out.println("<body>Hello World</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    public void destroy(){
+        super.destroy();
+    }
+}
